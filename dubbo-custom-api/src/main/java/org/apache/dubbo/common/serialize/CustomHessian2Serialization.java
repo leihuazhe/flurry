854c425ff49c08c36d518aefc38f955e769ef648
雷华哲 <leihz@yunjiglobal.com>
2019-07-17 12:13:00 +0800

    重构网关 V2

---
+++ dubbo-custom-api/src/main/java/org/apache/dubbo/common/serialize/CustomHessian2Serialization.java
@@ -0,0 +1,36 @@
+package org.apache.dubbo.common.serialize;
+
+import org.apache.dubbo.common.URL;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import static org.apache.dubbo.common.serialize.Constants.HESSIAN2_SERIALIZATION_ID;
+
+/**
+ * @author Denim.leihz 2019-07-10 6:20 PM
+ */
+public class CustomHessian2Serialization implements Serialization {
+
+    @Override
+    public byte getContentTypeId() {
+        return HESSIAN2_SERIALIZATION_ID;
+    }
+
+    @Override
+    public String getContentType() {
+        return "x-application/hessian2";
+    }
+
+    @Override
+    public ObjectOutput serialize(URL url, OutputStream out) throws IOException {
+        return new CustomHessian2ObjectOutput(out);
+    }
+
+    @Override
+    public ObjectInput deserialize(URL url, InputStream is) throws IOException {
+        return new CustomHessian2ObjectInput(is);
+    }
+
+}
