3130ed7f839ff89a6737023754f86718f8001286
雷华哲 <leihz@yunjiglobal.com>
2019-08-18 14:08:24 +0800

    修复序列化不兼容问题

--- gateway-core/src/main/java/org/apache/dubbo/common/serialize/CustomHessian2Serialization.java
+++ gateway-core/src/main/java/org/apache/dubbo/common/serialize/CustomHessian2Serialization.java
@@ -1,12 +1,16 @@
 package org.apache.dubbo.common.serialize;
 
 import org.apache.dubbo.common.URL;
+import org.apache.dubbo.common.serialize.hessian2.Hessian2ObjectInput;
+import org.apache.dubbo.common.serialize.hessian2.Hessian2ObjectOutput;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
 import static org.apache.dubbo.common.serialize.Constants.HESSIAN2_SERIALIZATION_ID;
+import static org.apache.dubbo.remoting.Constants.SERIALIZATION_KEY;
+import static org.apache.dubbo.util.GateConstants.SERIALIZATION_CUSTOM;
 
 /**
  * @author Denim.leihz 2019-07-10 6:20 PM
@@ -25,12 +29,18 @@ public class CustomHessian2Serialization implements Serialization {
 
     @Override
     public ObjectOutput serialize(URL url, OutputStream out) throws IOException {
-        return new CustomHessian2ObjectOutput(out);
+        if (SERIALIZATION_CUSTOM.equals(url.getParameter(SERIALIZATION_KEY))) {
+            return new CustomHessian2ObjectOutput(out);
+        }
+        return new Hessian2ObjectOutput(out);
     }
 
     @Override
     public ObjectInput deserialize(URL url, InputStream is) throws IOException {
-        return new CustomHessian2ObjectInput(is);
+        if (SERIALIZATION_CUSTOM.equals(url.getParameter(SERIALIZATION_KEY))) {
+            return new CustomHessian2ObjectInput(is);
+        }
+        return new Hessian2ObjectInput(is);
     }
 
 }
